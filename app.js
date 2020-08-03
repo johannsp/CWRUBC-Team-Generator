76187f034c3dc2985083be9c8e1154e36a5c941b
John L Spetz <JohnLSpetz@zoho.com>
2020-08-03 13:23:17 -0400

    Add classes from inputs as WIP

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -11,6 +12,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const getUI = require("./lib/getUserInput");
 const render = require("./lib/htmlRenderer");
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
 async function main() {
   // Write code to use inquirer to gather information about the development team members,
@@ -18,25 +20,29 @@ async function main() {
   const inputs = await getUI.getEmployeeDetailsAll();
   console.log(inputs);
 
-  // After the user has input all employees desired, call the `render` function (required
-  // above) and pass in an array containing all employee objects; the `render` function will
-  // generate and return a block of HTML including templated divs for each employee!
+  if (inputs) {
+    // After the user has input all employees desired, call the `render` function (required
+    // above) and pass in an array containing all employee objects; the `render` function will
+    // generate and return a block of HTML including templated divs for each employee!
+    const generatedHtml = render(inputs);
 
-  // After you have your html, you're now ready to create an HTML file using the HTML
-  // returned from the `render` function. Now write it to a file named `team.html` in the
-  // `output` folder. You can use the variable `outputPath` above target this location.
-  // Hint: you may need to check if the `output` folder exists and create it if it
-  // does not.
+    // After you have your html, you're now ready to create an HTML file using the HTML
+    // returned from the `render` function. Now write it to a file named `team.html` in the
+    // `output` folder. You can use the variable `outputPath` above target this location.
+    // Hint: you may need to check if the `output` folder exists and create it if it
+    // does not.
 
-  // HINT: each employee type (manager, engineer, or intern) has slightly different
-  // information; write your code to ask different questions via inquirer depending on
-  // employee type.
+    // HINT: each employee type (manager, engineer, or intern) has slightly different
+    // information; write your code to ask different questions via inquirer depending on
+    // employee type.
 
-  // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-  // and Intern classes should all extend from a class named Employee; see the directions
-  // for further information. Be sure to test out each class and verify it generates an
-  // object with the correct structure and methods. This structure will be crucial in order
-  // for the provided `render` function to work! ```
+    // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+    // and Intern classes should all extend from a class named Employee; see the directions
+    // for further information. Be sure to test out each class and verify it generates an
+    // object with the correct structure and methods. This structure will be crucial in order
+    // for the provided `render` function to work! ```
+    await writeFileAsync(outputPath, generatedHtml);
+  }
 }
 
-main()
+main();
